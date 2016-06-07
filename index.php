9e4fab2a6544ba9968d29d64032fd3e6cade3cb5
Oleg Zernov <olegfact@gmail.com>
2016-06-07 21:16:22 +0300

    Полное слияние

--- index.php
+++ index.php
@@ -73,44 +73,37 @@ $app->get('/calendar', function() use ($app) {
 // страница с регистрацией пользователя
 $app->match('/reg', function() use ($app) {
 
-    print_r($_POST);
-    if( !isset($_POST["password"]) || !isset($_POST["email"]) ||  !isset($_POST["name"]))
-    {
+    if ( isset($_POST["name"]) && isset($_POST["email"] ) && isset($_POST["password"]) && isset($_POST["password_repeat"])) {
+        $name = ( !preg_match("|[\\<>'\"-/]+|", $_POST["name"]) ) ? $_POST["name"] : false;
+        $email = ( preg_match("|^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$|", $_POST["email"]) ) ? $_POST["email"] : false;
+        // проверка что пароли равны
+        if ( $_POST["password"] == $_POST["password_repeat"]  ) {
+            $password = password_hash(trim($_POST["password_repeat"]), PASSWORD_DEFAULT);
+            if ( $name && $email && $password ) {
+                global $db;
+
+                // узнаем пользователь с таким  уже существует
+                $u_id = $db->query("
+                    SELECT
+                        `u_id`
+                    FROM
+                        `users`
+                    WHERE 
+                        `u_email` = '".$email."'
+                    LIMIT 1");
+                list( $user_id) = $u_id->fetchColumn();
+                if ( !$user_id ) {
+                    $sql ="INSERT INTO USERS (`u_name`, `u_password`, `u_email`, `role_id`, `u_create_date`, `u_active_date`)
+                         VALUES ('".$name."', '".$password."', '".$email."', 1, NOW(), NOW()); ";
+                    $db->query($sql) ;
+                    echo 'регистрация прошла успешно';
+                } else echo 'Пользователь с таким email уже существует';
+            } else echo 'Некорректные данные';
+
+        } else  echo "не равны пароли";
+    } else {
+
         echo "незаполнены нужные поля ";
-    }
-    else
-
-    if(preg_match("|[\\<>'\"-/]+|", $_POST["name"]))
-    {
-        //error
-        echo "error name ";
-    }else
-    if(!preg_match("|^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$|", $_POST["email"]))
-    {
-        //error
-        echo "error email ";
-    }else
-    if($_POST["password"]!=$_POST["password_repeat"] )
-    {
-        //error
-        echo "не равны пароли";
-
-    }else
-    {
-
-        global $db;
-
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        // ! проверка на существание емейла в бд
-        // предусмотреть уникальный и секретный код в куки (допустим сохрять в бд)
-
-        $password = password_hash(trim($_POST["password_repeat"]), PASSWORD_DEFAULT);
-        $sql ="INSERT INTO USERS (`u_name`, `u_password`, `u_email`, `role_id`, `u_create_date`, `u_active_date`)
-             VALUES ('".$name."', '".$password."', '".$email."', 1, NOW(), NOW()); ";
-        echo ($sql);
-        $db->query($sql) ;
-        echo 'регистрация прошла успешно';
     }
 
 
