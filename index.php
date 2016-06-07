07dd919095f5ca3f9afdbc5fa9cf6ad293ee96d3
Alisa Zdorenko <zdorenko.alisa@gmail.com>
2016-06-07 20:03:54 +0300

    Переделала регистрацию +добавила проверку

--- index.php
+++ index.php
@@ -4,12 +4,16 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $dsn="mysql:dbname=green_age;host=127.0.0.1"; $user_db="root"; $password_db="";
 
+global $db;
+$db = new PDO($dsn, $user_db, $password_db);
+$db->query("SET NAMES UTF8");
+
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/pages',
+    'twig.path' => __DIR__.'/pages',
 ));
 
 
@@ -26,9 +30,6 @@ $app->before(function ($request) use ($app) {
     $app['twig']->addGlobal('active', $request->get("_route"));
 });
 
-    global $dsn, $user_db, $password_db;
-    $db = new PDO($dsn, $user_db, $password_db);
-    $db->query("SET NAMES UTF8");
 
 //$app->before(function ($request) use ($app) {
 //    $app['twig']->addGlobal('active', $request->post("_route"));
@@ -42,8 +43,8 @@ $app->get('//', function() use ($app) {
         $events[]= $row;
     }
     $app['twig']->addGlobal('events', $events);
-//    print_r($events);
-	return $app['twig']->render('index.html');
+
+    return $app['twig']->render('index.html');
 })->bind('index');
 
 // создание события
@@ -59,45 +60,78 @@ $app->get('/calendar', function() use ($app) {
 // страница с регистрацией пользователя
 $app->match('/reg', function() use ($app) {
 
-    print_r($_POST);
-    if( !isset($_POST["password"]) || !isset($_POST["email"]) ||  !isset($_POST["name"]))
-    {
+    if ( isset($_POST["name"]) && isset($_POST["email"] ) && isset($_POST["password"]) && isset($_POST["password_repeat"])) {
+
+        $name = ( !preg_match("|[\\<>'\"-/]+|", $_POST["name"]) ) ? $_POST["name"] : false;
+        $email = ( preg_match("|^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$|", $_POST["email"]) ) ? $_POST["email"] : false; 
+
+        // проверка что пароли равны
+        if ( $_POST["password"] == $_POST["password_repeat"]  ) {
+            $password = password_hash(trim($_POST["password_repeat"]), PASSWORD_DEFAULT);
+
+            if ( $name && $email && $password ) {
+                    global $db;
+              
+                // узнаем пользователь с таким  уже существует
+                $u_id = $db->query("
+                    SELECT
+                        `u_id`
+                    FROM
+                        `users`
+                    WHERE 
+                        `u_email` = '".$email."'
+                    LIMIT 1");
+                list( $user_id) = $u_id->fetchColumn();
+
+                if ( !$user_id ) {
+
+                    $sql ="INSERT INTO USERS (`u_name`, `u_password`, `u_email`, `role_id`, `u_create_date`, `u_active_date`)
+                         VALUES ('".$name."', '".$password."', '".$email."', 1, NOW(), NOW()); ";
+                    $db->query($sql) ;
+                    echo 'регистрация прошла успешно';
+
+                } else echo 'Пользователь с таким email уже существует';
+
+            } else echo 'Некорректные данные';
+            
+        } else  echo "не равны пароли";
+
+    } else {
+        
         echo "незаполнены нужные поля ";
     }
-    else
 
-    if(preg_match("|[\\<>'\"-/]+|", $_POST["name"]))
-    {
-        //error
-        echo "error name ";
-    }else
-    if(!preg_match("|^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$|", $_POST["email"]))
-    {
-        //error
-        echo "error email ";
-    }else
-    if($_POST["password"]!=$_POST["password_repeat"] )
-    {
-        //error
-        echo "не равны пароли";
 
-    }else
-    {
 
-        global $db;
+    // if ( isset($_POST["password"]) && isset($_POST["password_repeat"]) ) 
+    // {
+    //     if ( isset($_POST["email"]) ||  isset($_POST["name"]) )
+    // }
+    // else
 
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        // ! проверка на существание емейла в бд
-        // предусмотреть уникальный и секретный код в куки (допустим сохрять в бд)
+    // if(preg_match("|[\\<>'\"-/]+|", $_POST["name"]))
+    // {
+    //     //error
+    //     echo "error name ";
+    // }else
+    // if(!preg_match("|^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$|", $_POST["email"]))
+    // {
+    //     //error
+    //     echo "error email ";
+    // }else
+    // if($_POST["password"]!=$_POST["password_repeat"] )
+    // {
+    //     //error
 
-        $password = password_hash(trim($_POST["password_repeat"]), PASSWORD_DEFAULT);
-        $sql ="INSERT INTO USERS (`u_name`, `u_password`, `u_email`, `role_id`, `u_create_date`, `u_active_date`)
-             VALUES ('".$name."', '".$password."', '".$email."', 1, NOW(), NOW()); ";
-        echo ($sql);
-        $db->query($sql) ;
-        echo 'регистрация прошла успешно';
-    }
+    // }else
+    // {
+
+       
+
+    //     // ! проверка на существание емейла в бд
+    //     // предусмотреть уникальный и секретный код в куки (допустим сохрять в бд)
+
+    // }
 
 
 	return $app['twig']->render('reg.html');
