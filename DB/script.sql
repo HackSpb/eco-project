43a7a9ef36036f323b05f8a138ca08631e4ce320
Alisa Zdorenko <zdorenko.alisa@gmail.com>
2016-07-25 20:03:27 +0300

    Подключила новый дизайн главной страницы

--- DB/script.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `events` CHANGE `ev_begin_date` `ev_begin_date` DATE NULL DEFAULT NULL COMMENT 'Дата начала';
-ALTER TABLE `events` CHANGE `ev_end_date` `ev_end_date` DATE NOT NULL COMMENT 'Дата окночания';
-ALTER TABLE `events` ADD `ev_begin_time` TIME NOT NULL COMMENT 'Время начала' AFTER `ev_begin_date`;
-ALTER TABLE `events` ADD `ev_end_time` TIME NOT NULL COMMENT 'Время окончания' AFTER `ev_end_date`;
--- map_files/checkExpiredEvents.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once '../lib/php/map_lib/MapRefresher.php';
-
-$refresh = new MapLib\MapRefresher();
-$refresh->refresh();
-
